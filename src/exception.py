f4cd74b35d5f20b1d6379794d04735256968bd01
Renaulthr <virbxr1352001@gmail.com>
2023-12-12 16:57:22 +0530

    EDA and Problem statement

--- src/exception.py
+++ src/exception.py
@@ -25,7 +25,7 @@ if __name__=="__main__":
     try:
         a=1/0
     except Exception as e:
-         logging.info("Divide by zero")
+         logging.info("Divide by Zero")
          raise CustomException(e,sys)
 
    
